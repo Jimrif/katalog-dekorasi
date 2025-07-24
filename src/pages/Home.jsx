67fd866193a7e913997ed8e4e683acb86264cec0
Jimrif <projectjiwa7@gmail.com>
2025-07-24 05:51:07 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
 
+function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Katalog Dekorasi</h1>
+      <p>Selamat datang!</p>
+    </div>
+  )
+}
+
+export default Home
