910d41e32760df04f9cdf67168ee9e0f052e75c4
Jimrif <projectjiwa7@gmail.com>
2025-07-24 05:01:34 +0000

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold">Login</h2>
+      <p>Silakan login sebagai staff atau owner.</p>
+    </div>
+  )
+}
+
+export default Login
